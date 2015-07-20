8ca38096686597e24fd1c63ea8ba83526a313b89
gregoryhbowler <greg@brudmusic.com>
2015-07-20 13:14:43 -0700

    fixed sign out issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    user_path(resource)
+    welcome_index_path
   end
 
   protected
