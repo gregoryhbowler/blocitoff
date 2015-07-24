e466e60552612d903825b4dd311af097bc022190
gregoryhbowler <greg@brudmusic.com>
2015-07-24 10:01:04 -0700

    added deletion, still need to add ajax destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,20 @@ class ItemsController < ApplicationController
     redirect_to current_user
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      flash[:notice] = "Item deleted successuflly!"
+    else
+      flash[:notice] = "Oops something went wrong, try again please"
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   def item_params
     params.require(:item).permit(:name)
   end
